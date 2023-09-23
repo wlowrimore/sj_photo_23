58417373fceee937847c9608c873f33901762a24
William Lowrimore <wlowrimore@gmail.com>
2023-09-23 01:35:53 -0500

    adds transition hover on buttons

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
           <h3 className='text-5xl font-thin tracking-wide leading-relaxed text-neutral-200 '>If you like what you see, and you are interested in purchasing a print, please reach out to me, and I would be happy to provide you with a quote.</h3>
         </div>
         <div id='button-group' className='flex justify-center gap-8 text-xl'>
-          <div className='border-[1px] border-opacity-40 border-neutral-300 py-1 px-3'>
+          <div className='border-[1px] border-opacity-40 border-neutral-500 py-1 px-3 transition 1500ms ease hover:bg-neutral-300 hover:bg-opacity-5'>
             <button type='button' className='text-neutral-300 tracking-widest'>Services</button>
           </div>
-          <div className='border-[1px] border-opacity-40 border-neutral-300 py-1 px-3'>
+          <div className='border-[1px] border-opacity-40 border-neutral-500 py-1 px-3 transition 1500ms ease hover:bg-neutral-300 hover:bg-opacity-5'>
             <button type='button' className='text-neutral-300 tracking-widest'>Location</button>
           </div>
         </div>
