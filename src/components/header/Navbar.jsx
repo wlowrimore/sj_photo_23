3eb3fc305ae169b3b3c0ed19cb42acaa2c39c196
William Lowrimore <wlowrimore@gmail.com>
2023-09-22 22:46:15 -0500

    sets up basic navbar structure with little styles

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  }
+
   return (
     <header className='bg-transparent text-white fixed top-0 w-full max-h-[5rem]'>
       <nav className='flex justify-between items-center py-6 px-20'>
@@ -8,16 +19,28 @@ const Navbar = () => {
           <h1 className='text-5xl text-emerald-700 tracing-widest'>Sayre Joan</h1>
           <h3 className='text-3xl text-emerald-100 opacity-70 tracking-wider relative z-[-1] bottom-6 left-14'>Photography</h3>
         </div>
+        {toggleNav && (
+          <div>
+            <ul className='flex space-x-8 tracking-wider uppercase cursor-pointer'>
+              <ul onMouseOver={handleMouseOver}>
+                <li className='hover:text-gray-400'>musicians</li>
+                {isHovering && (
+                  <ul onMouseOut={handleMouseOut} className='fixed flex text-sm mt-4 space-x-4 z-20'>
+                    <li className='hover:text-emerald-500 py-3'>lead vocals</li>
+                    <li className='hover:text-emerald-500 py-3'>strings</li>
+                    <li className='hover:text-emerald-500 py-3'>drums & percussion</li>
+                  </ul>
+                )}
+              </ul>
+              <li className='hover:text-gray-400'>Publications</li>
+              <li className='hover:text-gray-400'>Architecture</li>
+            </ul>
+          </div>
+        )}
         <div>
-          <ul className='flex space-x-6'>
-            <li>link</li>
-            <li>link</li>
-            <li>link</li>
-            <li>link</li>
-            <li>link</li>
-            <li>link</li>
-          </ul>
+          <h2 onClick={() => setToggleNav(!toggleNav)} className='uppercase tracking-wider cursor-pointer hover:text-gray-300'>menu</h2>
         </div>
+
       </nav>
     </header>
   )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='min-h-screen bg-maincustombg bg-fixed bg-no-repeat bg-cover'>
+    <div className='min-h-screen bg-maincustombg bg-fixed bg-no-repeat bg-cover bg-center'>
       <div className='text-4xl text-emerald-400'>
 
       </div>
