78573d9d58c68d9d19ffb122c4c92277b39564b9
William Lowrimore <wlowrimore@gmail.com>
2023-09-28 16:59:18 -0500

    updates musicians gallery modal code to match publications gallery modal code.

--- src/components/gallery/musicians_gallery/MusiciansGalleryCard.jsx
+++ src/components/gallery/musicians_gallery/MusiciansGalleryCard.jsx
@@ -32,7 +32,7 @@ const MusiciansGalleryCard = () => {
         </div>
         {modalOpen && (
 
-          <div className='w-screen h-screen fixed top-0 left-0 z-1000 bg-black/60 backdrop-blur-sm flex flex-col justify-center items-center px-auto' onClick={closeModal}>
+          <div className='w-screen h-screen fixed top-0 left-0 z-10 bg-black/60 backdrop-blur-sm flex flex-col justify-center items-center px-auto' onClick={closeModal}>
             <Fade duration={1000} triggerOnce="true">
               <img src={selectedImage} alt="Selected" className='w-[50rem] border-2 border-emerald-200 rounded shadow-md shadow-black' />
             </Fade>
